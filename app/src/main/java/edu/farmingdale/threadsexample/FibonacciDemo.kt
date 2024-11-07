61278401f756d16e156a3fd1408866d1cc2c787e
Whitman 216 <wh216@it.farmingdale.edu>
2024-11-07 15:05:49 -0500

    Nova's Changes

--- app/src/main/java/edu/farmingdale/threadsexample/FibonacciDemo.kt
+++ app/src/main/java/edu/farmingdale/threadsexample/FibonacciDemo.kt
@@ -2,6 +2,8 @@ package edu.farmingdale.threadsexample
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -12,8 +14,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import java.text.NumberFormat
 import java.util.Locale
@@ -22,8 +27,11 @@ import java.util.Locale
 fun FibonacciDemoNoBgThrd() {
     var answer by remember { mutableStateOf("") }
     var textInput by remember { mutableStateOf("40") }
+    val coroutineScope = rememberCoroutineScope()
+    var fibonacciJob : Job by remember { mutableStateOf(Job()) }
 
     Column {
+        Spacer(Modifier.padding(60.dp))
         Row {
             TextField(
                 value = textInput,
@@ -35,9 +43,10 @@ fun FibonacciDemoNoBgThrd() {
                 )
             )
             Button(onClick = {
-                val num = textInput.toLongOrNull() ?: 0
+                fibonacciJob = coroutineScope.launch(Dispatchers.Default){
+                    val num = textInput.toLongOrNull() ?: 0
                 val fibNumber = fibonacci(num)
-                answer = NumberFormat.getNumberInstance(Locale.US).format(fibNumber)
+                answer = NumberFormat.getNumberInstance(Locale.US).format(fibNumber)}
             }) {
                 Text("Fibonacci")
             }
--- app/src/main/java/edu/farmingdale/threadsexample/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/threadsexample/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import edu.farmingdale.threadsexample.countdowntimer.CountDownActivity
 import edu.farmingdale.threadsexample.ui.theme.ThreadsExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ThreadsExampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting("Android", Modifier.padding(innerPadding))
+                    FibonacciDemoNoBgThrd()
                 }
             }
         }
